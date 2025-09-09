620e545c525a2b5ed115e7a6b5dc7448c6cd6f1d
Walson124 <walson124@gmail.com>
2025-09-09 09:37:03 -0400

    adjusted setquerieddata for new response

--- src/app/analysis/tab-0/stack-analysis.js
+++ src/app/analysis/tab-0/stack-analysis.js
@@ -27,7 +27,7 @@ export const StackAnalysis = () => {
             if (response.data.length == 0) {
                 return;
             }
-            setQueriedData(response.data);
+            setQueriedData(response.data.stacked);
         }).catch((error) => {
             console.error("Error fetching stack chart options:", error);
         });
